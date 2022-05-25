e7c67a1fdecd0262705712f7122a61317011e65b
Will Neal <willwneal@gmail.com>
2022-05-25 19:48:37 -0400

    Added Listing and Realtor models. Generated Migrations

--- real_estate_project/wnre/settings.py
+++ real_estate_project/wnre/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'wnre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'realestate_db',
+        'USER': 'postgres',
+        'PASSWORD': 'temporarypostgres',
+        'HOST': 'localhost'
     }
 }
 
