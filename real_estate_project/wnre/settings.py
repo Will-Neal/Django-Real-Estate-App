ab305a8340dc60f2a661e695081e97cf60bd89fb
Will Neal <willwneal@gmail.com>
2022-06-03 09:16:19 -0400

    Added listings and realtors/generated and linked media folder

--- real_estate_project/wnre/settings.py
+++ real_estate_project/wnre/settings.py
@@ -129,6 +129,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'wnre/static')
 ]
 
+#media folder settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
