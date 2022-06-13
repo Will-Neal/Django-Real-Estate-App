bc393a12b36e6d0bf6c626758d6c325e4097deb2
Will Neal <willwneal@gmail.com>
2022-06-13 17:57:51 -0400

    Added fields to  realtor and listings tables on admin page

--- real_estate_project/listings/admin.py
+++ real_estate_project/listings/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Listing
 
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'realtor', 'address', 'list_Date', 'price')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'realtor', 'description', 'address', 'city', 'state', 'zipcode', 'price')
+    list_per_page = 25
+
+admin.site.register(Listing, ListingAdmin)
