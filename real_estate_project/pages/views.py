e6bb7c974d5b6b58f27a7f451822facf9085d38c
Will Neal <willwneal@gmail.com>
2022-06-16 19:11:44 -0400

    Added realtor info to about card

--- real_estate_project/pages/views.py
+++ real_estate_project/pages/views.py
@@ -1,7 +1,9 @@
+from numbers import Real
 from django.shortcuts import render
 from django.http import HttpResponse
 
 from listings.models import Listing
+from realtors.models import Realtor
 
 def index(request):
     listings = Listing.objects.order_by('-list_Date').filter(is_published=True)[:3]
@@ -13,7 +15,13 @@ def index(request):
     return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors
+    }
+
+    return render(request, 'pages/about.html', context)
 
 # def listings(request):
 #     return render(request, 'pages/listings.html')
